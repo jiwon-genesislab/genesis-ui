909599a7e5d6b4be192ccecfa9b0886f7c4d1dae
jiwoniy <jiwoniy@genesislab.ai>
2019-12-17 08:05:16 +0900

    init

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+// import external from 'rollup-plugin-peer-deps-external'
+// import json from 'rollup-plugin-json';
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  '@material-ui/core': '@material-ui/core'
+};
+
+const babelOptions = {
+  exclude: /node_modules/,
+  // We are using @babel/plugin-transform-runtime
+  runtimeHelpers: true,
+  configFile: './babel.config.js',
+};
+
+const commonjsOptions = {
+  ignoreGlobal: true,
+  include: /node_modules/,
+  namedExports: {
+    'prop-types': [
+      'elementType',
+      'bool',
+      'func',
+      'object',
+      'oneOfType',
+      'element',
+    ],
+    'react-is': [
+      'ForwardRef',
+      'isFragment',
+      'isLazy',
+      'isMemo',
+      'isValidElementType',
+    ],
+  },
+};
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'esm',
+    // globals
+  },
+  external: Object.keys(globals),
+  plugins: [
+    // external(),
+    resolve(),
+    // babel({
+    //   // presets: ['@babel/preset-react'],
+    //   runtimeHelpers: true,
+    //   exclude: 'node_modules/**'
+    // }),
+    babel(babelOptions),
+    commonjs(commonjsOptions),
+    // json(),
+  ]
+};
